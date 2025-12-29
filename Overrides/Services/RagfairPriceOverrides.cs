083c9c2f5cb347aee4a13638bc658c1f22157c87
minesettimi <minesettimi@gmail.com>
2025-12-29 02:37:27 -0500

    dynamic pricing override

--- Overrides/Services/RagfairPriceOverrides.cs
+++ Overrides/Services/RagfairPriceOverrides.cs
@@ -1,14 +1,41 @@
 using System.Reflection;
+using FleaSimulator.Services;
 using SPTarkov.Reflection.Patching;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Services;
 
 namespace FleaSimulator.Overrides.Services;
 
 
-public class GetDynamicOfferPriceOverride : AbstractPatch
+public class GetDynamicPriceOverride : AbstractPatch
 {
+    private static PriceService priceService;
+    private static PresetService presetService;
+    
     protected override MethodBase? GetTargetMethod()
     {
+        priceService = ServiceLocator.ServiceProvider.GetService<PriceService>()!;
+        presetService = ServiceLocator.ServiceProvider.GetService<PresetService>()!;
         return typeof(RagfairPriceService).GetMethod(nameof(RagfairPriceService.GetDynamicItemPrice));
+    }
+    
+    //can't override the middle and the functions used don't have enough params, replace it
+    [PatchPrefix]
+    public static bool PatchPrefixAttribute(MongoId tplId,
+        MongoId desiredCurrency,
+        Item? item,
+        IEnumerable<Item>? offerItems,
+        bool? isPackOffer,
+        ref double __result
+        )
+    {
+        if (!presetService.Config.Core.BuyConfig.Enabled)
+            return true;
+            
+        __result = priceService.GetItemPrice(tplId, desiredCurrency, item, offerItems, isPackOffer);
+        
+        return false;
     }
 }
--- Services/PriceService.cs
+++ Services/PriceService.cs
@@ -1,8 +1,14 @@
+using System.Reflection;
+using FleaSimulator.Models.Config;
 using FleaSimulator.Models.State;
 using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Helpers;
 using SPTarkov.Server.Core.Models.Common;
 using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Enums;
+using SPTarkov.Server.Core.Models.Spt.Config;
 using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Servers;
 using SPTarkov.Server.Core.Services;
 using SPTarkov.Server.Core.Utils;
 
@@ -12,9 +18,20 @@ namespace FleaSimulator.Services;
 public class PriceService(DatabaseService database,
     ItemDataService dataService,
     MathUtil mathUtil,
+    RagfairPriceService ragfairPriceService,
+    ConfigServer configServer,
+    PresetHelper presetHelper,
+    HandbookHelper handbookHelper,
+    RandomUtil randomUtil,
+    ItemDataService itemService,
+    ItemHelper itemHelper,
     ISptLogger<PriceService> logger
     )
 {
+    private readonly RagfairConfig _ragfairConfig = configServer.GetConfig<RagfairConfig>();
+    private MethodBase _getWeaponPresetprice = typeof(RagfairPriceService)
+        .GetMethod("GetWeaponPresetPrice", BindingFlags.Instance | BindingFlags.NonPublic)!;
+    
     public void UpdatePrices()
     {
         logger.Info("[FleaSimulator] Updating offer prices.");
@@ -37,5 +54,48 @@ public class PriceService(DatabaseService database,
             priceTable[states.Key] = Math.Round(mathUtil.MapToRange(currentState.UpdateTime, currentState.LastUpdate,
                 currentState.NextUpdate,minPrice, maxPrice));
         }
+    }
+
+    public double GetItemPrice(MongoId tplId,
+        MongoId desiredCurrency,
+        Item? item,
+        IEnumerable<Item>? offerItems,
+        bool? isPackOffer
+        )
+    {
+        double price = ragfairPriceService.GetFleaPriceForItem(tplId);
+
+        if (item?.Upd?.SptPresetId is not null
+            && offerItems is not null
+            && presetHelper.IsPresetBaseClass(item.Upd.SptPresetId.Value, BaseClasses.WEAPON))
+        {
+            price = (double)_getWeaponPresetprice.Invoke(ragfairPriceService, [item, offerItems, price])!;
+        }
+
+        if (item is not null && !_ragfairConfig.Dynamic.IgnoreQualityPriceVarianceBlacklist.Contains(tplId))
+        {
+            double qualityModifier = itemHelper.GetItemQualityModifier(item);
+            price *= qualityModifier;
+        }
+        
+        ItemState itemState = itemService.CurrentState.Items[tplId];
+        CategoryConfig category = itemState.Category;
+
+        double minPrice = category.MinOfferPrice * 100;
+        double maxPrice = category.MaxOfferPrice * 100;
+
+        double multiplier = randomUtil.GetBiasedRandomNumber(minPrice, 
+            maxPrice,
+            maxPrice - minPrice - category.Chaos,
+            2d + category.Chaos*2);
+        
+        price *= multiplier / 100;
+
+        if (desiredCurrency != Money.ROUBLES)
+        {
+            price = handbookHelper.FromRoubles(price, desiredCurrency);
+        }
+        
+        return price <= 0 ? 0.1d : price;
     }
 }
