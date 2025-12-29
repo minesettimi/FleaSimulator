e2450fd09ee6cb181cd1324d68dd36e5e4ff01be
minesettimi <minesettimi@gmail.com>
2025-12-29 01:42:22 -0500

    use real flea prices over handbook

--- Overrides/Servers/RagfairServerOverrides.cs
+++ Overrides/Servers/RagfairServerOverrides.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using FleaSimulator.Services;
 using SPTarkov.Reflection.Patching;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Extensions;
@@ -11,14 +12,20 @@ namespace FleaSimulator.Overrides.Servers;
 
 public class UpdateOverride : AbstractPatch
 {
+    private static PresetService _presetService;
+    
     protected override MethodBase? GetTargetMethod()
     {
+        _presetService = ServiceLocator.ServiceProvider.GetRequiredService<PresetService>()!;
         return typeof(RagfairServer).GetMethod(nameof(RagfairServer.Update));
     }
 
     [PatchPrefix]
     public static bool Prefix(RagfairServer __instance)
     {
+        if (!_presetService.Config.Core.BuyConfig.Enabled)
+            return true;
+        
         //kill off every fake flea offer before processing
         RagfairOfferHolder offerHolder = ServiceLocator.ServiceProvider.GetRequiredService<RagfairOfferHolder>();
         List<MongoId> expired = offerHolder.GetStaleOfferIds();
