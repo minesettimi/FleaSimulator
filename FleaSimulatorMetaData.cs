4c7c4a13c50ac2fbb1e7d2849fc2caa9f01c1e7d
minesettimi <minesettimi@gmail.com>
2026-01-01 01:29:18 -0500

    forge release prep

--- FleaSimulatorMetaData.cs
+++ FleaSimulatorMetaData.cs
@@ -12,13 +12,13 @@ public static class FleaSimulatorMetaData
         public override string Name { get; init; } = "Advanced Flea Market Simulator";
         public override string Author { get; init; } = "minesettimi";
         public override List<string>? Contributors { get; init; }
-        public override Version Version { get; init; } = new(0, 5, 2);
-        public override Range SptVersion { get; init; } = new("~4.0.0");
+        public override Version Version { get; init; } = new(0, 6, 0);
+        public override Range SptVersion { get; init; } = new("~4.0.4");
         
         public override List<string>? Incompatibilities { get; init; } = ["xyz.drakia.livefleaprices", "com.acidphantasm.delayedfleasales"];
         public override Dictionary<string, Range>? ModDependencies { get; init; }
-        
-        public override string? Url { get; init; }
+
+        public override string? Url { get; init; } = "https://github.com/minesettimi/FleaSimulator";
         public override bool? IsBundleMod { get; init; }
         public override string License { get; init; } = "MIT";
     }
